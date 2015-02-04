a64ec701e2a90a6bad988e973873adc3e912cbfe
menglingjun <menglingjun@baidu.com>
2015-02-05 02:00:41 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,29 @@
+/**
+ * @file test
+ * @author junmer
+ */
+
+'use strict';
+var assert = require('assert');
+var fs = require('fs');
+var isOtf = require('./');
+
+it('should detect OTF from String', function () {
+    assert(isOtf(
+        fs.readFileSync('FontAwesome.otf').toString('binary')
+    ));
+});
+
+it('should detect OTF from Buffer', function () {
+    assert(isOtf(fs.readFileSync('FontAwesome.otf')));
+});
+
+it('should be false when detect TTF file', function () {
+    assert(!isOtf(fs.readFileSync('FontAwesome-webfont.ttf')));
+});
+
+it('should be false when detect other type file', function () {
+    assert(!isOtf(fs.readFileSync('index.js')));
+    assert(!isOtf(fs.readFileSync('package.json')));
+});
+
